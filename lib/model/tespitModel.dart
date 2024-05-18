48f7effe8e5a0fa1c625e179324a5fa65df0514b
berkebalci <berkebalcii38@gmail.com>
2024-05-19 00:45:27 +0300

    New files has been added

---
+++ lib/model/tespitModel.dart
@@ -0,0 +1,16 @@
+class TespitModel {
+  int id;
+  String x;
+  String y;
+  DateTime tespitZamani;
+  String tespitFotografi;
+
+  TespitModel(
+    this.id,
+    this.x,
+    this.y,
+    this.tespitZamani,
+    this.tespitFotografi
+  );
+  
+}
