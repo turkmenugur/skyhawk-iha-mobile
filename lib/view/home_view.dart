09db5fe225f1b9d32a3f9c4ea46ca7149d3c7f46
Uğur Türkmen <trkmn.ugur.12@gmail.com>
2024-05-20 00:15:37 +0300

    Home page designed

---
+++ lib/view/home_view.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:skyhawk_iha/view/map_view.dart';
+
+class HomeView extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Skyhawk'),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              itemCount: 10, // Listenizdeki öğe sayısı
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Icon(Icons.list),
+                  title: Text('Öğe $index'),
+                  subtitle: Text('Alt başlık $index'),
+                  onTap: (){
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => MapView()));
+                  },
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            /*child: ElevatedButton(
+              onPressed: () {
+                // Butonun yapacağı işlemler
+              },
+              child: Text('Orta Buton'),
+            ),*/
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 50.0,
+          child: Center(
+            child: ElevatedButton(
+              onPressed: () {
+                // Butonun yapacağı işlemler
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)
+                ),
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.phone, color: Colors.white),
+                  SizedBox(width: 8),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
