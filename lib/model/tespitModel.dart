83b8633394e20d2e60d87861a4a6641c998c7416
berkebalci <berkebalcii38@gmail.com>
2024-05-21 11:14:15 +0300

    tespitService has been modified

--- lib/model/tespitModel.dart
+++ lib/model/tespitModel.dart
@@ -5,16 +5,23 @@ class TespitModel {
   DateTime tespitZamani;
   String tespitFotografi;
 
-  TespitModel(
-    this.id,
-    this.x,
-    this.y,
-    this.tespitZamani,
-    this.tespitFotografi
-  );
+  TespitModel(this.id, this.x, this.y, this.tespitZamani, this.tespitFotografi);
 
-  factory TespitModel.fromJson(Map <String,dynamic> json){
-    return TespitModel(json['id'], json['x'], y, tespitZamani, tespitFotografi) //TODO: yapilacak
+  factory TespitModel.fromJson(Map<String, dynamic> json) {
+    return TespitModel(json['id'], json['xKoordinatı'], json['ykoordinatı'], 
+    json['tespitZamani'],json['tespitFotografi']); //TODO: yapilacak
   }
+
+  Map<String,dynamic> toJson(){
+    Map<String,dynamic> json = {};
+    json['id'] = id;
+    json['xKoordinatı'] = x;
+    json['ykoordinatı'] = y;
+    json['tespitZamani'] = tespitZamani;
+    json['tespitFotografi'] = tespitFotografi;
+
+    return json;
+    }
+
   
 }
--- lib/service/tespitService.dart
+++ lib/service/tespitService.dart
@@ -1,6 +1,26 @@
-class tespitService{
-  Future getTespitData(){
-    
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
-  }  
+class tespitService {
+  Future getAllTespitData() async {
+    //İlerde kullanılacak.
+    Response response = await http
+        .post(Uri.parse("http://localhost:5180/api/Tespit/GetAllTespit"));
+
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      return false;
+    }
+  }
+
+  Future getTespitby() async {
+    Response response =
+        await http.post(Uri.parse("http://localhost:5180/api/Tespit/"));
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      return false;
+    }
+  }
 }
